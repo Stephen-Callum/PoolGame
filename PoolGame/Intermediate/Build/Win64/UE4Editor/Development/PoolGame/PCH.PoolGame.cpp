e5845cffa6d89963c1b5b1d4ef6893066eb605fd
Owen Alister <owen.alister@live.co.uk>
2017-05-27 18:45:21 +0100

    Initial commit

---
+++ PoolGame/Intermediate/Build/Win64/UE4Editor/Development/PoolGame/PCH.PoolGame.cpp
@@ -0,0 +1,1 @@
+#include "D:/Unreal Projects/PoolGame/PoolGame/Intermediate/Build/Win64/UE4Editor/Development/PoolGame/PCH.PoolGame.h"
---
+++ PoolGame/Source/PoolGame/PoolGameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PoolGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POOLGAME_API APoolGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
